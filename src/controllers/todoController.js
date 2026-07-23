d2ed5ea1373a8c97dec64b00a42542e1dc681ca2
Dawid Kumor <dawidkumor@tuta.io>
2026-07-23 23:53:46 +0200

    feat: PATCH /task/:id - update tasks

--- src/controllers/todoController.js
+++ src/controllers/todoController.js
@@ -30,4 +30,18 @@ async function deleteTodo(req, res) {
   }
 }
 
-module.exports = { createTodo, getTodos, deleteTodo };
+async function updateTodo(req, res) {
+  try {
+    const result = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!result) {
+      return res.status(404).json({ message: "Document not found" });
+    }
+    res.status(200).json({ message: `${result._id} updated` });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+}
+
+module.exports = { createTodo, getTodos, deleteTodo, updateTodo };
--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -4,10 +4,12 @@ const {
   createTodo,
   getTodos,
   deleteTodo,
+  updateTodo,
 } = require("../controllers/todoController");
 
 router.post("/", createTodo);
 router.get("/", getTodos);
 router.delete("/:id", deleteTodo);
+router.patch("/:id", updateTodo);
 
 module.exports = router;
